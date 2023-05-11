defe4aa1fa244f9165b6d38922242fe7ee11160a
Your Name <you@example.com>
2023-05-11 17:12:26 +0100

    Updating show date logic code

--- script.js
+++ script.js
@@ -7,22 +7,31 @@ const paragraph = document.getElementById("paragraph");
 button.addEventListener('click', (event) => {
     const newToDo = document.createElement("li");
     newToDo.innerText = input.value;
-    list.appendChild(newToDo);
-    input.value = '';
+    if (input.value != ""){
+        list.appendChild(newToDo);
+        input.value = '';
 
-    const deleteButton = document.createElement("button");
-    deleteButton.innerText = "delete item";
-    list.appendChild(deleteButton);
-
-    deleteButton.addEventListener('click', (event) =>{
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "delete item";
+        list.appendChild(deleteButton);
+   
+         deleteButton.addEventListener('click', (event) =>{
         list.removeChild(newToDo);
         list.removeChild(deleteButton);
-    })
+        });
+    }
+    
 })
 
 showDate.addEventListener('click', (event) =>{
     const showDate = document.createElement("p");
-    showDate.innerText = new Date;
-    paragraph.appendChild(showDate);
+    // if (paragraph.value == null)}
+        showDate.innerText = new Date;
+        paragraph.appendChild(showDate);
+        const newDate = showDate.getAttribute("data-showDate");
+        const datePresent = newDate === "true";
+        const booleanInvented = !datePresent;
+        showDate.setAttribute("data-showDate", booleanInvented);
+    
 });
 
