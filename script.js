9e8880f75a8b17d99ebdfb4c73801973b9e4f25e
Sarah Abdulle <sarahabdulle1@yahoo.com>
2023-05-11 14:52:49 +0100

    planning lab

---
+++ script.js
@@ -0,0 +1,27 @@
+const input = document.getElementById("new-todo");
+const button = document.getElementById("enter");
+const list = document.getElementById("list");
+button.addEventListener("click", event => {
+// 1: get input under id new-todo - getAttribute
+  
+    // const userInput = input.getAttribute("data-userInput");
+    // const isStringInputted = userInput === "";
+    // const boolStringInput = !userInput;
+    // list.setAttribute()
+    // console.log()
+
+
+
+    // const newToDoList = document.createElement("li");
+    // newToDoList.textContent= input;
+    // console.log(list);
+
+
+
+// check if there is any input already - check if it is not null 
+// assign input that we get using getAttribute to a variable that is a list -createElement
+
+// delete button: remove item for the list
+});
+
+
