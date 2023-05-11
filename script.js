38967b467ed22552f17e548e93d620b2d9ceaa2e
Your Name <you@example.com>
2023-05-11 16:32:25 +0100

    Allow user to delete item and show date

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const button = document.querySelector("#enter");
 const list = document.querySelector("#list");
-const input = document.getElementById("new-todo")
+const input = document.getElementById("new-todo");
+const showDate = document.getElementById("show-date");
+const paragraph = document.getElementById("paragraph");
 
 button.addEventListener('click', (event) => {
     const newToDo = document.createElement("li");
     newToDo.innerText = input.value;
-    
-
     list.appendChild(newToDo);
-    
     input.value = '';
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "delete item";
+    list.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', (event) =>{
+        list.removeChild(newToDo);
+        list.removeChild(deleteButton);
+    })
 })
+
+showDate.addEventListener('click', (event) =>{
+    const showDate = document.createElement("p");
+    showDate.innerText = new Date;
+    paragraph.appendChild(showDate);
+});
+
